3590b268187f81c42649957921cfd243a882516c
rayile0630 <rayile1024@gmail.com>
2019-11-24 17:09:24 +0000

    Poststukuttatoko

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_145417) do
+ActiveRecord::Schema.define(version: 2019_11_24_170514) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "qmemo"
+    t.text "amemo"
+    t.string "address"
+    t.boolean "comprehension"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -21,4 +33,5 @@ ActiveRecord::Schema.define(version: 2019_11_24_145417) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
